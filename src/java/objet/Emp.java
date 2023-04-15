671af46f44952f7cfb52c76099fbb2ffa7dba025
RABEARISOA Sedera <119619625+Sedera53@users.noreply.github.com>
2023-04-15 04:23:24 +0200

    Add files via upload

---
+++ src/java/objet/Emp.java
@@ -0,0 +1,29 @@
+package objet;
+
+import utils.ModelView;
+import utils.MyAnnotation;
+
+
+public class Emp {
+    
+    @MyAnnotation(url="/Framework/parler")
+    public ModelView parler(){
+        ModelView m = new ModelView();
+        m.setView("liste.jsp");
+        return m;
+    }
+    
+    @MyAnnotation(url="get-emp")
+    public ModelView getAll(){
+        ModelView m = new ModelView();
+        m.setView("liste.jsp");
+        return m;
+    }
+    
+    @MyAnnotation(url="add-emp")
+    public ModelView insert(){
+        ModelView m = new ModelView();
+        m.setView("liste.jsp");
+        return m;
+    }
+}
